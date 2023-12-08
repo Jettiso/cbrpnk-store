65066e3d59cf0d49b4fcba6ae167b0863366fde9
Jettiso <ilmsmjettison@gmail.com>
2023-12-08 23:35:50 +0800

    fix deprecated images.domain to remotePatterns

--- cbrpnk/next.config.js
+++ cbrpnk/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		// Replace "images.domains" with "images.remotePatterns"
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
